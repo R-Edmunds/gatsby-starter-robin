1c065bace39b63aa09761e6b782f5e02a5883dc4
Robin Edmunds <r.edmunds@live.co.uk>
2020-08-20 17:01:38 +0100

    Add example redux-form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Layout from "../templates/Layout"
+import ReduxFormExample from "../components/ReduxFormExample"
 import hamster from "../images/hamster.jpg"
 
 const LandingPage = () => (
@@ -7,6 +8,8 @@ const LandingPage = () => (
     <h1>gatsby-starter-robin</h1>
     <p>Custom strapi, sass, redux, gatsby boilerplate</p>
     <img src={hamster} alt="hamster" />
+    <hr />
+    <ReduxFormExample />
   </Layout>
 )
 
